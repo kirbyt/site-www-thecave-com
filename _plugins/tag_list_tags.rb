8bc1dd8806145881c3406e5dc2562340b1a2cafa
Kirby Turner <kirby@thecave.com>
2015-09-21 16:51:34 -0400

    Fix tag URL.

--- _plugins/tag_list_tags.rb
+++ _plugins/tag_list_tags.rb
@@ -6,7 +6,7 @@ module Jekyll
       tags.sort.each do |tag|
         posts_in_tag = context.registers[:site].tags[tag].size
         tag_dir = context.registers[:site].config['tag_dir']
-        tag_url = File.join(tag_dir, tag.gsub(/_|\P{Word}/, '%20').gsub(/-{2,}/, '-').downcase)
+        tag_url = File.join(tag_dir, tag.gsub(/_|\P{Word}/, '-').gsub(/-{2,}/, '-').downcase)
         html << "<li><a href='/#{tag_url}/'>#{tag} (#{posts_in_tag})</a></li>\n"
       end
       html
