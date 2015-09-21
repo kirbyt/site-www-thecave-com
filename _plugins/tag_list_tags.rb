cf729cd38e7d52fd18bd1585d4c857cd320ed0b3
Kirby Turner <kirby@thecave.com>
2015-09-21 16:47:21 -0400

    Add tag list.

---
+++ _plugins/tag_list_tags.rb
@@ -0,0 +1,17 @@
+module Jekyll
+  class TagListTag < Liquid::Tag
+    def render(context)
+      html = ""
+      tags = context.registers[:site].tags.keys
+      tags.sort.each do |tag|
+        posts_in_tag = context.registers[:site].tags[tag].size
+        tag_dir = context.registers[:site].config['tag_dir']
+        tag_url = File.join(tag_dir, tag.gsub(/_|\P{Word}/, '%20').gsub(/-{2,}/, '-').downcase)
+        html << "<li><a href='/#{tag_url}/'>#{tag} (#{posts_in_tag})</a></li>\n"
+      end
+      html
+    end
+  end
+end
+
+Liquid::Template.register_tag('tag_list', Jekyll::TagListTag)
